943b70e31eb5d82a50ba32d2c95c4a847bafd5d2
Oliver Reinking <oliver@codingjungle.de>
2023-10-31 08:56:10 +0100

    npm install durchgeführt, nodes vorher komplett gelöscht.

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 npm install
 
 git add .
-git commit -m "npm install durchgeführt, nodes vorher komplett gelöscht"
+git commit -m "npm install durchgeführt, nodes vorher komplett gelöscht."
 git push
